ed9f8f7cb30e8515fc8bb2bbaa8a0cfae0b40a41
SinisterSup <mheist456@gmail.com>
2025-08-05 09:42:54 +0530

    refactor to Optimize "builder" logic

--- src/builder/Main.java
+++ src/builder/Main.java
@@ -4,13 +4,20 @@ public class Main {
     public static void main(String[] args) {
         // Creating/Building a Student Class using a StudBuilder,
         // is readable & manageable, since we have setters for each attribute;
-        Builder stud1Builder = new Builder();
-        stud1Builder.setId(1);
-        stud1Builder.setName("Ram");
-//        stud1Builder.setAge(31);
-        stud1Builder.setPsp(89.7);
-//        stud1Builder.setGradYear(2016);
-        stud1Builder.setGradYear(2018);
+
+//        // Since Builder class is static and belongs within Student Class,
+//        // The code below throws compile time errors…
+//        Builder stud1Builder = new Builder();
+//        stud1Builder.setId(1);
+//        stud1Builder.setName("Ram");
+// //     stud1Builder.setAge(31);
+//        stud1Builder.setPsp(89.7);
+// //     stud1Builder.setGradYear(2016);
+//        stud1Builder.setGradYear(2018);
+//        Student stud1 = new Student(stud1Builder);
+//        stud1.solveProblem();
+//        // The above initialization won't work since the
+//        // Student Constructor is private.
 
 //        Builder studentBuilder = Student.getBuilder();
 //        studentBuilder.setId(2);
@@ -28,8 +35,6 @@ public class Main {
                 .setGradYear(2022)
                 .build();
 
-        Student stud1 = new Student(stud1Builder);
-        stud1.solveProblem();
         stud2.solveProblem();
     }
 }
--- src/builder/Student.java
+++ src/builder/Student.java
@@ -13,7 +13,12 @@ public class Student {
         return new Builder();
     }
 
-    public Student(Builder builder) {
+    // Right now since this is public - anyone can call the Student Constructor.
+    // Since we want to only allow Construction of the object using builder,
+    // this can be made `private`, & then be called only from within the
+    // same class using the static builder class within.!
+//    public Student(Builder builder) {
+    private Student(Builder builder) {
         this.id = builder.getId();
         this.name = builder.getName();
         this.age = builder.getAge();
@@ -32,5 +37,57 @@ public class Student {
     }
     void takeTest() {
         System.out.println(name + " took a test! ");
+    }
+
+
+    public static class Builder {
+        private Integer id;
+        private String name;
+        private int age;
+        private double psp;
+        private Integer gradYear;
+
+        public Builder setId(Integer id) {
+            this.id = id;
+            return this;
+        }
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+        public Builder setPsp(double psp) {
+            this.psp = psp;
+            return this;
+        }
+        public Builder setGradYear(Integer gradYear) {
+            this.gradYear = gradYear;
+            return this;
+        }
+        public Integer getId() {
+            return this.id;
+        }
+        public String getName() {
+            return this.name;
+        }
+        public int getAge() {
+            return this.age;
+        }
+        public double getPsp() {
+            return this.psp;
+        }
+        public Integer getGradYear() {
+            return this.gradYear;
+        }
+
+        // the Student object is built
+        // by making use of Private Constructor of Student from within!!
+        public Student build() {
+            return new Student(this);
+        }
+
     }
 }
